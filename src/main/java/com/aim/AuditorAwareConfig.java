245d946a47fb7185447cdd221f5e7edac4f76ae8
duddnd11 <yjk8003@gmail.com>
2025-07-11 00:23:17 +0900

    config

---
+++ src/main/java/com/aim/AuditorAwareConfig.java
@@ -0,0 +1,44 @@
+package com.aim;
+
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class AuditorAwareConfig {
+	
+	@Bean
+    public AuditorAware<String> auditorProvider() {
+        return new SpringSecurityAuditorAware();
+    }
+
+    static class SpringSecurityAuditorAware implements AuditorAware<String> {
+        @Override
+        public Optional<String> getCurrentAuditor() {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if (authentication == null || !authentication.isAuthenticated()) {
+                return Optional.empty(); // 인증되지 않은 경우 빈 Optional 반환
+            }
+
+            // 현재 로그인한 사용자 이름을 가져옴
+            String username;
+            if (authentication.getPrincipal() instanceof UserDetails) {
+                username = ((UserDetails) authentication.getPrincipal()).getUsername();
+                log.info("username: {}",username);
+            } else {
+                username = authentication.getPrincipal().toString();
+            }
+
+            return Optional.of(username);
+        }
+    }
+}
---
+++ src/main/java/com/aim/SchedulerConfig.java
@@ -0,0 +1,27 @@
+package com.aim;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulerConfig {
+
+	@Bean
+	public TaskScheduler taskScheduler() {
+		ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5); // 스레드 풀 크기 설정
+        scheduler.setThreadNamePrefix("ScheduledTask-"); // 스레드 이름 접두사 설정
+        scheduler.initialize(); // 스케줄러 초기화
+        return scheduler;
+	}
+	
+	@Bean
+	public ScheduledExecutorService scheduledExecutorService() {
+		return Executors.newScheduledThreadPool(20);
+	}
+}
