cf853ce0ec7c1eed2bc713796da877a0abd1fef2
duddnd11 <yjk8003@gmail.com>
2025-08-27 21:06:40 +0900

    Score findByGameId 쿼리 이미지 관련 수정

--- src/main/java/com/aim/infrastructure/game/ScoreJpaRepository.java
+++ src/main/java/com/aim/infrastructure/game/ScoreJpaRepository.java
@@ -11,8 +11,17 @@ import com.aim.domain.game.entity.Score;
 import com.aim.domain.member.entity.Member;
 
 public interface ScoreJpaRepository extends JpaRepository<Score,Long>{
-	@Query("select s from Score s join fetch s.member m left join m.profileImg p "
-			+ "where s.game.gameId=:gameId and (p.type = 'PROFILE' OR p IS NULL) and s.pvp.pvpId is null order by s.totalScore desc")
+	@Query("select s from Score s "
+			+ "join fetch s.member m "
+			+ "left join m.profileImg p "
+			+ "where s.game.gameId=:gameId and "
+			+ "(p.uploadFileId = ("
+			+ "select max(p2.uploadFileId) "
+			+ "from UploadFile p2 "
+			+ "where p2.member = m "
+			+ "and p2.type = 'PROFILE' "
+			+ ") or p is null) "
+			+ "and s.pvp.pvpId is null order by s.totalScore desc")
 	Slice<Score> findByGameId(@Param("gameId") Long gameId, Pageable pageable);
 	
 	Slice<Score> findByGame_GameIdAndMember_MemberId(Long gameId, Long memberId, Pageable pageable);
@@ -25,3 +34,4 @@ public interface ScoreJpaRepository extends JpaRepository<Score,Long>{
 			+ "where s.member=:member")
 	ScoreCountResult findByScoreCount(@Param("member") Member member);
 }
+	
